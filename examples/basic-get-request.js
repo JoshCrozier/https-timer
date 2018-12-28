44ba1e995a5a70da945fe03feb7f2ecf85c6450d
Josh Crozier <crozier4@gmail.com>
2018-12-27 19:41:18 -0500

    Add example showing the module usage

---
+++ examples/basic-get-request.js
@@ -0,0 +1,12 @@
+const httpsTimer = require('./../index.js');
+
+httpsTimer.get('https://www.google.com', (error, response) => {
+  if (!error && response) {
+    console.log('Headers: ', response.headers);
+    console.log('Status Code: ', response.statusCode);
+    console.log('Response body: ', response.body);
+    console.log('Response Timing: ', response.timing);
+  } else {
+    console.log('Request error: ', error);
+  }
+});
---
+++ examples/get-request-custom-headers.js
@@ -0,0 +1,17 @@
+const httpsTimer = require('./../index.js');
+
+const url = 'https://api.github.com/repos/JoshCrozier/https-timer';
+const options = {
+  headers: {
+    'User-Agent': 'HTTPS Request Timer'
+  }
+};
+
+httpsTimer.get(url, options, (error, response) => {
+  if (!error && response && response.statusCode === 200) {
+    console.log('Response body: ', JSON.parse(response.body));
+    console.log('Response Timing: ', response.timing);
+  } else {
+    console.log('Request error: ', error);
+  }
+});
